6a18d205c6c7d13d6b7c275e12d040738a1d6505
Ian Torres <iantorres@outlook.com>
2023-08-28 14:28:00 -0400

    Generate UUID on user creating.

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserObserver
+{
+    /**
+     * Handle the User "creating" event.
+     */
+    public function creating(User $user): void
+    {
+        $user->uuid = Str::uuid();
+    }
+
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
