0806b1fe38753fcda70df87be4ae6007a0fed830
Ian Torres <iantorres@outlook.com>
2023-08-28 10:18:46 -0400

    Migrations added.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->char('uuid',36);
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->tinyInteger('is_admin')->default(0);
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->char('avatar',36)->nullable();
+            $table->string('address',255);
+            $table->string('phone_number',255);
+            $table->tinyInteger('is_marketing')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
