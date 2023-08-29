5c11e60126b94962173f3172e773ef7d32096dde
Ian Torres <iantorres@outlook.com>
2023-08-28 20:23:36 -0400

    User factory fields added.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,12 @@ class UserFactory extends Factory
     {
         return [
             'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'address' => fake()->address,
+            'phone_number' => fake()->phoneNumber,
         ];
     }
 
