d8d23b53a712456e294c63ece5f80f2adb99c905
Ian Torres <iantorres@outlook.com>
2023-08-28 20:24:03 -0400

    Admin API requests added.

---
+++ app/Http/Requests/Api/v1/Admin/LogoutRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Api\v1\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LogoutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check() && auth()->user()->is_admin;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
---
+++ app/Http/Requests/Api/v1/Admin/UserDeleteRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Api\v1\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserDeleteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check() && auth()->user()->is_admin && ! $this->route('user')->is_admin;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
