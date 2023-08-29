3c138e6dc832d8c1b1662c3e600d7292787741ad
Ian Torres <iantorres@outlook.com>
2023-08-28 20:21:59 -0400

    File model and migration added.

---
+++ database/migrations/2023_08_26_140549_create_files_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->char('uuid',36);
+            $table->string('name',255);
+            $table->string('path',255);
+            $table->string('size',255);
+            $table->string('type',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
